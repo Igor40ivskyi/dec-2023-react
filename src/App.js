ff94f4ed587c796c7d22a2156517d208e40e0c7a
Admin <sorokivskyi777@gmail.com>
2023-05-16 13:02:52 +0300

    context learning

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import {useEffect, useReducer} from "react";
+
+const reducer = (state, action) => {
+    console.log(state);
+    console.log(action);
+    return 'xxx';
+};
 
 function App() {
+    const [state, dispatch] = useReducer(reducer, {count: 0});
+
+    console.log(state);
+
+    useEffect(() => {
+        dispatch(233);
+    }, []);
+
     return (
         <div className="App">
             App
