5b06d9f4283bea58dc464e9d3285483d285b8c3e
Admin <sorokivskyi777@gmail.com>
2023-05-20 11:42:54 +0300

    base redux learning

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import {useDispatch, useSelector} from "react-redux";
+import AAA from "./AAA";
 
 function App() {
+
+
+    const dispatch = useDispatch();
+
+    const increment = () => {
+        dispatch({type: 'INC', payload: 5});
+    };
+
+    const decrement = () => {
+        dispatch({type: 'DEC'});
+    };
+
+    const reset = () => {
+        dispatch({type: 'RESET'});
+    };
+
+
     return (
         <div className="App">
-            App
+            <button onClick={increment}>incremet</button>
+            <button onClick={decrement}>decrement</button>
+            <button onClick={reset}>reset</button>
+            <AAA/>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Provider} from "react-redux";
 import App from './App';
+import {createStore} from "redux";
+
+const baseReducer = (state=0,action   ) => {
+    switch (action.type) {
+        case 'INC':
+            return state = state + action.payload;
+        case 'DEC':
+            return --state;
+        case 'RESET':
+            return 0;
+        default:
+            return state;
+    }
+};
+
+const store = createStore(baseReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App/>
+    <Provider store={store}>
+        <App/>
+    </Provider>
 );
 
