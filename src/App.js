c92744840d43068cbb1d90ad1287f840395f3bbd
Admin <sorokivskyi777@gmail.com>
2023-05-23 19:02:59 +0300

    base redux learning

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+
+
+const fetchCars = () => async (dispatch) => {
+
+        dispatch({type: 'SET_LOADING'});
+
+    const response = await fetch('http://owu.linkpc.net/carsAPI/v1/cars')
+        .then(value => value.json());
+
+    dispatch({type: 'SET_CARS', payload: response});
+
+};
 
 function App() {
+
+    const state = useSelector(state => state);
+    const dispatch = useDispatch();
+
+
+    useEffect(() => {
+
+        dispatch(fetchCars());
+
+        // dispatch({type:'SET_LOADING'})
+
+    }, [dispatch]);
+
+
+
     return (
         <div className="App">
-            App
+            {state.isLoading && <h2>LOADING.......</h2>}
+
+            {
+                state.cars.map(value => <div key={value.id}>{value.brand}</div>)
+            }
+
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {applyMiddleware, createStore} from "redux";
+import {Provider} from "react-redux";
+import thunk from "redux-thunk";
+
+let initialState = {
+    cars: [],
+    isLoading: false
+};
+
+const carReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_CARS':
+            return {...state, cars: [...action.payload], isLoading: false};
+        case 'SET_LOADING':
+            return {...state, isLoading: !state.isLoading};
+        default:
+            return state
+    }
+};
+
+const store = createStore(carReducer, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App/>
+    <Provider store={store}>
+        <App/>
+    </Provider>
 );
 
