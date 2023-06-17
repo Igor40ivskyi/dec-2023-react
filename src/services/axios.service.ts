aeb84292f0591517bca0987a9b77ff015ba88552
Admin <sorokivskyi777@gmail.com>
2023-06-17 13:12:13 +0300

    lesson9 auth

--- src/services/axios.service.ts
+++ src/services/axios.service.ts
@@ -1,8 +1,10 @@
-import axios from "axios";
+import axios, {AxiosError} from "axios";
 import {baseURL} from "../constants";
 import {authService} from "./auth.service";
 
 const axiosService = axios.create({baseURL});
+
+let isRefreshing = false;
 
 axiosService.interceptors.request.use(req => {
     const access = authService.getAccessToken();
@@ -16,22 +18,26 @@ axiosService.interceptors.response.use(
     res => {
         return res
     },
-    async error =>{
+    async (error: AxiosError) => {
         const originalRequest = error.config;
-        if (error.response.status === 401 && !originalRequest._isRefreshing) {
-            originalRequest._isRefreshing = true;
-            try {
-                await authService.refresh();
-                return axiosService(originalRequest);
-            }catch (e) {
-                authService.deleteTokens();
-                return Promise.reject(error);
+        if (error.response.status === 401) {
+            if (!isRefreshing) {
+                isRefreshing = true;
+                try {
+                    await authService.refresh();
+                    isRefreshing = false;
+                    return axiosService(originalRequest);
+                }catch(e) {
+                    authService.deleteTokens();
+                    isRefreshing = false;
+                    return Promise.reject();
+                }
             }
-
+            return Promise.reject(error);
         }
         return Promise.reject(error);
     }
-    );
+);
 
 export {
     axiosService
