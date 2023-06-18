705bbf70ee53b7fde00b228ece406d524b7609bc
Admin <sorokivskyi777@gmail.com>
2023-06-18 13:46:41 +0300

    lesson9 auth practice

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,17 @@
 import {NavLink} from "react-router-dom";
 import './Header.css';
+import {useAppSelector} from "../../hooks";
 
 const Header = () => {
+
+    const {me} = useAppSelector(state => state.authReducer);
+
+
     return (
         <div className={'header'}>
+            {
+                me && <div className={'userIcon'}>{me.username}</div>
+            }
             <NavLink to={'login'}>Login</NavLink>
             <NavLink to={'register'}>Register</NavLink>
         </div>
